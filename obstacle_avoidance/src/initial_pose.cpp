db922187040bc8fd2d1eed7f22da802c4c28d3d6
Muhammad Labiyb Afakh <labiybafakh@gmail.com>
2024-03-03 18:57:59 +0900

    add initialpose node to adjust map pose

---
+++ obstacle_avoidance/src/initial_pose.cpp
@@ -0,0 +1,45 @@
+#include "ros/ros.h"    
+#include "geometry_msgs/PoseWithCovarianceStamped.h"
+
+
+
+int main(int argc, char** argv){
+
+    ros::init(argc, argv, "init_pose");
+
+    ros::NodeHandle nh;
+    ros::Publisher pose_publisher = nh.advertise<geometry_msgs::PoseWithCovarianceStamped>("initialpose", 50);
+
+    ros::Rate rate(20);
+
+    //checking whether the initial_pose has activated or not.
+    while(pose_publisher.getNumSubscribers() < 1){
+        ROS_INFO("Waiting intialpose subscriber....");
+        rate.sleep();
+    }
+
+    for(int i=0; i<5; i++){
+        geometry_msgs::PoseWithCovarianceStamped initial_pose;
+
+        initial_pose.header.frame_id        = "map";
+        initial_pose.header.stamp           = ros::Time::now();
+        initial_pose.pose.pose.position.x   = -2.0;
+        initial_pose.pose.pose.position.y   = -0.5;
+        initial_pose.pose.pose.position.z   = 0;
+        initial_pose.pose.pose.orientation.x= 0;
+        initial_pose.pose.pose.orientation.y= 0;
+        initial_pose.pose.pose.orientation.z= 0;
+        initial_pose.pose.pose.orientation.w= 1;
+
+        pose_publisher.publish(initial_pose);
+        ROS_INFO("PUBLISHED");
+
+        ros::spinOnce();
+        rate.sleep();
+    }
+
+
+
+    return 0;
+
+}
